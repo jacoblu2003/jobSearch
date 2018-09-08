cb6978b9cc2609f89184298d571520fa7cf69bdc
Jacob Lu <zhenli@live.ca>
2018-09-08 00:51:47 -0400

    Adding webpack

---
+++ web/webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "src/index.html",
+  filename: "index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin],
+  output: {
+      path: path.resolve(__dirname, '../static')
+  }
+};
